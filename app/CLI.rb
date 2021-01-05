591e36586668847a2d32b2478acba44df6c88318
Elvis Landi <elandi13@yahoo.com>
2021-01-05 11:02:53 -0500

    More Cli update

--- app/CLI.rb
+++ app/CLI.rb
@@ -27,7 +27,7 @@ class CLI
 
   attr_accessor :user, :restaurant, :user_restaurant, :restaurant_location, :location, :style, :restaurant_style
 
-  @@prompt = TTY::Prompt.new
+  @prompt = TTY::Prompt.new
 
 
   def self.welcome
@@ -43,7 +43,7 @@ class CLI
       choices = { "Log In" => 1,
           "Sign Up" => 2
       }
-      choice = @@prompt.select("Would you like to sign up or log in?", choices)
+      choice = @prompt.select("Would you like to sign up or log in?", choices)
       if choice == 1
           CLI.login
       elsif choice == 2
@@ -52,13 +52,36 @@ class CLI
     end
 
     def self.login
-       user = @@prompt.ask "Please enter Username"
-       sleep (0.5)
-       password = @@prompt.mask "Please enter Password"
-       return_user = User.all.select{|user| user if user.username == user && user.password == password}
-        puts "Welcome back #{user.capitalize}!"
+        username = @prompt.ask("What is your Username?")
+        password = @prompt.mask("What is your Password?")
+        if User.find_by(username: username, password: password)
+            @user = User.find_by(username: username, password: password)
+            @user
+        else 
+            puts "Unknown User or Password"
+            sleep(1)
+            system ('clear')
+            CLI.login_menu
+        end
     end
 
+    def self.signup
+        username = @prompt.ask ("Please create a Username")
+        while User.find_by(username: username) 
+            puts ("Sorry Username is Taken")
+            username = @prompt.ask ("Please create a Username")
+        end
+        # pizza = @@prompt.decorate(‘🍕 ‘)
+        password = @prompt.mask ("Please create a Password")  
+        User.create(username: username, password: password)
+    end
+
+    def self.main_menu
+
+    end
+
+end
 
 
-  end
+
+#use for signup  hungry = prompt.yes?("Are You Hungry?")
--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,5 @@ require_relative '../config/environment'
 require "tty-prompt"
 
 CLI.welcome
-#sleep (1) *use later on 
 CLI.login_menu
 
