ddced0328f5a204882a15b23b06be34c625162ff
Elvis Landi <elandi13@yahoo.com>
2021-01-05 13:50:57 -0500

    assocations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,27 @@
+require 'pry'
 class User < ActiveRecord::Base
 # add associatons!
     has_many :user_restaurants
     has_many :restaurants, through: :user_restaurants
 
     attr_accessor :favorites 
-    @favorites = []
+    @@favorites = []
 
+    def user_restaurants 
+        self.restaurants.uniq 
+    end
+
+    def restaurant_name
+        binding.pry
+        user_restaurants.map{|rest| rest.name}
+    end
 
 
     def add_restaurant_to_favorites(restaurant)
-        UserRestaurant.create(user_id: self.id, restaurant_id: restaurant.id)
+        #binding.pry
+        favorite = UserRestaurant.create(user_id: self.id, restaurant_id: restaurant.id)
+        
+        @@favorite << favorite 
     end
 
     # def delete_restaurant_from_favorites(restaurant)
